4447ae420d72bdcdd97066767cf9ef00d52a331d
natan.rauber <rauber@protonmail.com>
2021-08-25 19:07:10 -0300

    Initial commit

---
+++ main/bitmap.h
@@ -0,0 +1,99 @@
+//============================================================================
+// Name        : arquivoPPM.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const int bytesPerPixel = 3; /// red, green, blue
+const int fileHeaderSize = 14;
+const int infoHeaderSize = 40;
+unsigned char *img;
+int line_size;
+int cel_size;
+
+char *createBitmapFileHeader(int height, int width, int paddingSize)
+{
+	int fileSize = fileHeaderSize + infoHeaderSize + (bytesPerPixel * width + paddingSize) * height;
+
+	static unsigned char fileHeader[] = {
+		0, 0,		/// signature
+		0, 0, 0, 0, /// image file size in bytes
+		0, 0, 0, 0, /// reserved
+		0, 0, 0, 0, /// start of pixel array
+	};
+
+	fileHeader[0] = (unsigned char)('B');
+	fileHeader[1] = (unsigned char)('M');
+	fileHeader[2] = (unsigned char)(fileSize);
+	fileHeader[3] = (unsigned char)(fileSize >> 8);
+	fileHeader[4] = (unsigned char)(fileSize >> 16);
+	fileHeader[5] = (unsigned char)(fileSize >> 24);
+	fileHeader[10] = (unsigned char)(fileHeaderSize + infoHeaderSize);
+
+	return (char *)fileHeader;
+}
+
+char *createBitmapInfoHeader(int height, int width)
+{
+	static unsigned char infoHeader[] = {
+		0, 0, 0, 0, /// header size
+		0, 0, 0, 0, /// image width
+		0, 0, 0, 0, /// image height
+		0, 0,		/// number of color planes
+		0, 0,		/// bits per pixel
+		0, 0, 0, 0, /// compression
+		0, 0, 0, 0, /// image size
+		0, 0, 0, 0, /// horizontal resolution
+		0, 0, 0, 0, /// vertical resolution
+		0, 0, 0, 0, /// colors in color table
+		0, 0, 0, 0, /// important color count
+	};
+
+	infoHeader[0] = (unsigned char)(infoHeaderSize);
+	infoHeader[4] = (unsigned char)(width);
+	infoHeader[5] = (unsigned char)(width >> 8);
+	infoHeader[6] = (unsigned char)(width >> 16);
+	infoHeader[7] = (unsigned char)(width >> 24);
+	infoHeader[8] = (unsigned char)(height);
+	infoHeader[9] = (unsigned char)(height >> 8);
+	infoHeader[10] = (unsigned char)(height >> 16);
+	infoHeader[11] = (unsigned char)(height >> 24);
+	infoHeader[12] = (unsigned char)(1);
+	infoHeader[14] = (unsigned char)(bytesPerPixel * 8);
+
+	return (char *)infoHeader;
+}
+
+void writeBitmapFile(ofstream &out, int width, int height)
+{
+	char padding[3] = {0, 0, 0};
+	char color[3] = {0, 0, 0};
+	int paddingSize = (4 - (width * bytesPerPixel) % 4) % 4;
+
+	char *fileHeader = createBitmapFileHeader(height, width, paddingSize);
+	char *infoHeader = createBitmapInfoHeader(height, width);
+
+	out.write((char *)fileHeader, fileHeaderSize);
+	out.write((char *)infoHeader, infoHeaderSize);
+
+	for (int y = height - 1; y > 0; y--)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			int p = (height - y) * line_size + x * cel_size;
+			color[2] = img[p];
+			color[1] = img[p + 1];
+			color[0] = img[p + 2];
+			out.write(color, sizeof(color));
+		}
+		out.write(padding, paddingSize);
+	}
+}
---
+++ main/main.cc
@@ -0,0 +1,98 @@
+
+
+#include "rtweekend.h"
+
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "bitmap.h"
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+color ray_color(const ray &r, const hittable &world)
+{
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec))
+    {
+        return 0.5 * (rec.normal + color(1, 1, 1));
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+int main()
+{
+
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    img = new unsigned char[image_width * image_height * 3 * sizeof(int)];
+    cel_size = sizeof(unsigned char) * 3;
+    line_size = cel_size * image_width;
+
+    // Read world from file
+    ifstream infile("input.txt");
+    string myText;
+    hittable_list world;
+
+    while (getline(infile, myText))
+    {
+        double arr[4];
+        int i = 0;
+        stringstream ss(myText);
+        while (ss.good() && i < 4)
+        {
+            ss >> arr[i];
+            ++i;
+        }
+
+        world.add(make_shared<sphere>(point3(arr[0], arr[1], arr[2]), arr[3]));
+    }
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+    // Render
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        for (int i = 0; i < image_width; ++i)
+        {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r, world);
+
+            int ir = static_cast<int>(255.999 * pixel_color.x());
+            int ig = static_cast<int>(255.999 * pixel_color.y());
+            int ib = static_cast<int>(255.999 * pixel_color.z());
+
+            unsigned char *pos = &img[j * line_size + i * cel_size];
+
+            pos[0] = (unsigned char)(ir);
+            pos[1] = (unsigned char)(ig);
+            pos[2] = (unsigned char)(ib);
+        }
+    }
+
+    ofstream outfile;
+    outfile.open("image.bmp", ios::binary | ios::out);
+    writeBitmapFile(outfile, image_width, image_height);
+    outfile.close();
+}
