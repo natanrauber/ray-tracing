b27755c5cdd7193b14bb054bf4d401524eb42aa3
natan.rauber <rauber@protonmail.com>
2021-08-25 20:06:37 -0300

    Add antialiasing

--- lib/main.cc
+++ lib/main.cc
@@ -1,17 +1,17 @@
 
 
-#include "src/rtweekend.h"
-#include "src/color.h"
-#include "src/ray.h"
-#include "src/vec3.h"
-#include "src/hittable_list.h"
-#include "src/sphere.h"
 #include "src/bitmap.h"
+#include "src/camera.h"
+#include "src/hittable_list.h"
+#include "src/ray.h"
+#include "src/rtweekend.h"
+#include "src/sphere.h"
+#include "src/vec3.h"
 
-#include <string>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -27,22 +27,11 @@ color ray_color(const ray &r, const hittable &world)
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-int main()
+hittable_list readWorld(string fileName)
 {
-
-    // Image
-    const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
-    const int image_height = static_cast<int>(image_width / aspect_ratio);
-
-    img = new unsigned char[image_width * image_height * 3 * sizeof(int)];
-    cel_size = sizeof(unsigned char) * 3;
-    line_size = cel_size * image_width;
-
-    // Read world from file
-    ifstream infile("input.txt");
-    string myText;
+    ifstream infile(fileName);
     hittable_list world;
+    string myText;
 
     while (getline(infile, myText))
     {
@@ -58,29 +47,52 @@ int main()
         world.add(make_shared<sphere>(point3(arr[0], arr[1], arr[2]), arr[3]));
     }
 
-    // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
+    return world;
+}
 
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+int main()
+{
+
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+
+    img = new unsigned char[image_width * image_height * 3 * sizeof(int)];
+    cel_size = sizeof(unsigned char) * 3;
+    line_size = cel_size * image_width;
+
+    // Read world from file
+    hittable_list world = readWorld("input.txt");
+
+    // Camera
+    camera cam;
 
     // Render
     for (int j = image_height - 1; j >= 0; --j)
     {
         for (int i = 0; i < image_width; ++i)
         {
-            auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height - 1);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r, world);
+            color pixel_color(0, 0, 0);
+
+            for (int s = 0; s < samples_per_pixel; ++s)
+            {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
 
             int ir = static_cast<int>(255.999 * pixel_color.x());
             int ig = static_cast<int>(255.999 * pixel_color.y());
             int ib = static_cast<int>(255.999 * pixel_color.z());
+
+            // Divide the color by the number of samples.
+            auto scale = 1.0 / samples_per_pixel;
+            ir *= scale;
+            ig *= scale;
+            ib *= scale;
 
             unsigned char *pos = &img[j * line_size + i * cel_size];
 
